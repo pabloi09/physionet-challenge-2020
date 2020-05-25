97bb613e6a2a1fcb9441659a2995b75e95294a8c
Pablo Martin Redondo <pablo.arribas100@gmail.com>
2020-05-25 19:44:35 +0200

    Changed script

--- webtool/run_servers.sh
+++ webtool/run_servers.sh
@@ -1,8 +1,5 @@
 sudo docker kill --force $(sudo docker ps -a -q)
 sudo docker image rm --force $(sudo docker image ls -a -q)
 sudo docker system prune
-cd frontend
-npm run build
-cd ..
 sudo docker-compose build
 sudo docker-compose up
